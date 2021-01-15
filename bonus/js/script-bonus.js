68e9c1b55969905591e3448a484daf5a3d7a660f
sabrina-fontana <sabfon@hotmail.it>
2021-01-15 18:52:00 +0100

    bonus completo

--- bonus/js/script-bonus.js
+++ bonus/js/script-bonus.js
@@ -1,43 +1,15 @@
-// Un alert espone 5 numeri casuali diversi.
-// Dopo 30 secondi l’utente deve inserire, un prompt alla volta, i numeri che ha visto precedentemente.
-// Una volta inseriti i 5 numeri, il software dice quanti e quali numeri sono stati ricordati.
-
 // FUNZIONI
 function randomNumber(min, max) {
   var result = Math.floor(Math.random() * (max + 1 - min) + min);
   return result;
 }
 
-function chiediNumero() {
-  $('.wait').removeClass('active');
-  $('.input-numbers').addClass('active');
-
-  // for (x = 0; x < 5; x++) {
-  //   // numeroScelto = parseInt(prompt('Inserisci i numeri che hai visto, uno alla volta'));
-  //   // if (isNaN(numeroScelto)) {
-  //   //   alert('Inserisci un valore corretto');
-  //   // } else {
-  //   // numeriUtente.push(numeroScelto);
-  //   // console.log(numeroScelto);
-  //   }
-  // }
-}
-//
-// function risultato() {
-//   for (var x = 0; x < numeriComputer.length; x++) {
-//     if (numeriComputer[x] === numeriUtente[x]) {
-//       contatoreRisultato += 1;
-//     }
-//   }
-//   console.log('Il risultato è ' + contatoreRisultato);
-//   alert('Hai indovinato ' + contatoreRisultato + ' numeri su 5\n' + 'I numeri che hai scritto sono ' + numeriUtente + '\nI numeri del computer sono ' + numeriComputer);
-// }
-
 // VARIABILI
 var numeroRandom;
 var arrayRandom = [];
 var numeriComputer = [];
 var numeroScelto;
+var arrayUtente = [];
 var numeriUtente = [];
 var contatoreRisultato = 0;
 
@@ -53,13 +25,32 @@ while (arrayRandom.length < 5) {
 console.log(numeriComputer);
 $('.random-numbers').text(numeriComputer);
 
-// dopo 30 secondi chiedo all'utente di riscrivere i numeri
-$('.conferma').click(function() {
+// INIZIO GIOCO al click su Fatto
+$('.start-game .conferma').click(function() {
   $('.start-game').hide();
   $('.wait').addClass('active');
-  setTimeout(chiediNumero, 3000);
+  setTimeout(function() {
+    $('.wait').removeClass('active');
+    $('.input-numbers').addClass('active');
+  }, 3000);
 });
 
-// confronto numeriComputer e numeriUtente e dò il risultato
-// deve essere dopo 30 secondi altrimenti non conosco i numeri scelti dall'utente
-// setTimeout(risultato, 30000);
+// INPUT al click su Conferma raccolgo gli input dell'utente
+$('.input-numbers .conferma').click(function() {
+  $('.input-numbers').removeClass('active');
+  for (var x = 1; x <= 5; x++) {
+    arrayUtente.push(parseInt($('.input-' + x).val()));
+    numeriUtente = arrayUtente.join(', ');
+  }
+  console.log(numeriUtente);
+  $('.result').addClass('active');
+  for (var x = 0; x < arrayRandom.length; x++) {
+    if (arrayRandom[x] === arrayUtente[x]) {
+      contatoreRisultato += 1;
+    }
+  }
+  $('.result .title').text(contatoreRisultato);
+  $('.user-numbers').append(numeriUtente);
+  $('.computer-numbers').append(numeriComputer);
+
+});
